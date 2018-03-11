656d6fe7e94c9f0da0d37feaf4081a95aa59d9e4
doublescale <doublescale@tutanota.com>
2018-03-11 22:37:01 +0100

    cleanup in Render.Mesh

--- src/Render/Mesh.hs
+++ src/Render/Mesh.hs
@@ -26,65 +26,48 @@ import Mesh
 import Render.Texture (loadTexture)
 import Render.Types (MeshDescriptor(..))
 
--- TODO: Try without [] case.
 loadMeshSequence ::
      MonadIO io => AnimationData Half TriFaces -> io MeshDescriptor
 loadMeshSequence
   AnimationData
   {meshSequence = MeshSequence {meshConstant = MeshConstant {..}, ..}, ..}
   =
-  case meshFrames of
-    [] ->
-      return
-        MeshDescriptor
-        {meshVaos = VG.empty, meshTexObj = GL.TextureObject 0, meshNIdxs = 0}
-    frames ->
-      liftIO $ do
-        bindUVs <- loadVbo (VVector (VS.map toV vertexUVs))
-        binders <-
-          V.fromList <$>
-          zipWithM
-            (\i MeshFrame {..} -> do
-               putStrLn ("Loading " ++ show i ++ "/" ++ show (length frames))
-               bindPositions <- loadVbo (VVector (VS.map toV vertexPositions))
-               bindNormals <- loadVbo (VVector (VS.map toV vertexNormals))
-               return (bindPositions, bindNormals))
-            [1 :: Int ..]
-            frames
-        vaos <-
-          zipWithM
-            (\i MeshFrame {} -> do
-               vao :: GL.VertexArrayObject <- GL.genObjectName
-               GL.bindVertexArrayObject $= Just vao
-               loadFaceIndices
-               let (bindPositions0, bindNormals0) = binders V.! i
-               let (bindPositions1, bindNormals1) =
-                     binders V.! ((i + 1) `mod` nFrames)
-               bindPositions0 0
-               bindPositions1 1
-               bindNormals0 2
-               bindNormals1 3
-               bindUVs 4
-               return vao)
-            [0 :: Int ..]
-            frames
-        meshTexObj <- loadTexture textureRGBData
-        return
-          MeshDescriptor
-          { meshVaos = VG.fromList vaos
-          , meshTexObj
-          , meshNIdxs =
-              fromIntegral (dim (toV (VS.head faces)) * VS.length faces)
-          }
-  where
-    nFrames = length meshFrames
-    sizeOfVec xs = fromIntegral (sizeOf (VS.head xs) * VS.length xs)
-    loadFaceIndices = do
-      ibo :: GL.BufferObject <- GL.genObjectName
-      GL.bindBuffer GL.ElementArrayBuffer $= Just ibo
-      let facesSize = sizeOfVec faces
-      VS.unsafeWith faces $ \ptr ->
-        GL.bufferData GL.ElementArrayBuffer $= (facesSize, ptr, GL.StaticDraw)
+  liftIO $ do
+    bindUVs <- loadVbo (VVector (VS.map toV vertexUVs))
+    binders <-
+      V.fromList <$>
+      zipWithM
+        (\i MeshFrame {..} -> do
+           putStrLn ("Loading " ++ show i ++ "/" ++ show (length meshFrames))
+           bindPositions <- loadVbo (VVector (VS.map toV vertexPositions))
+           bindNormals <- loadVbo (VVector (VS.map toV vertexNormals))
+           return (bindPositions, bindNormals))
+        [1 :: Int ..]
+        meshFrames
+    vaos <-
+      zipWithM
+        (\i MeshFrame {} -> do
+           vao :: GL.VertexArrayObject <- GL.genObjectName
+           GL.bindVertexArrayObject $= Just vao
+           _ <- loadBuffer GL.ElementArrayBuffer faces
+           let (bindPositions0, bindNormals0) = binders V.! i
+           let (bindPositions1, bindNormals1) =
+                 binders V.! ((i + 1) `mod` length meshFrames)
+           bindPositions0 0
+           bindPositions1 1
+           bindNormals0 2
+           bindNormals1 3
+           bindUVs 4
+           return vao)
+        [0 :: Int ..]
+        meshFrames
+    meshTexObj <- loadTexture textureRGBData
+    return
+      MeshDescriptor
+      { meshVaos = VG.fromList vaos
+      , meshTexObj
+      , meshNIdxs = fromIntegral (dim (toV (VS.head faces)) * VS.length faces)
+      }
 
 deleteMeshSequence :: MonadIO io => MeshDescriptor -> io ()
 deleteMeshSequence MeshDescriptor {meshVaos, meshTexObj} =
@@ -92,15 +75,28 @@ deleteMeshSequence MeshDescriptor {meshVaos, meshTexObj} =
     GL.deleteObjectNames (VG.toList meshVaos)
     GL.deleteObjectName meshTexObj
 
+loadBuffer :: Storable a => GL.BufferTarget -> VS.Vector a -> IO GL.BufferObject
+loadBuffer bufferTarget srcData = do
+  bufObj :: GL.BufferObject <- GL.genObjectName
+  GL.bindBuffer bufferTarget $= Just bufObj
+  let dataSize = sizeOfVec srcData
+  VS.unsafeWith srcData $ \ptr ->
+    GL.bufferData bufferTarget $= (dataSize, ptr, GL.StaticDraw)
+  return bufObj
+  where
+    sizeOfVec xs = fromIntegral (sizeOf (VS.head xs) * VS.length xs)
+
 data VVector = forall (n :: Nat) a. (Dim n, ToGLType a, Storable a) =>
   VVector (VS.Vector (V n a))
 
+class ToGLType a where toGLType :: Proxy a -> GL.DataType
+instance ToGLType Half where toGLType _ = GL.HalfFloat
+instance ToGLType Float where toGLType _ = GL.Float
+instance ToGLType Double where toGLType _ = GL.Double
+
 loadVbo :: VVector -> IO (GL.GLuint -> IO ())
 loadVbo (VVector (vvec :: VS.Vector (V n a))) = do
-  vbo :: GL.BufferObject <- GL.genObjectName
-  GL.bindBuffer GL.ArrayBuffer $= Just vbo
-  withVecSize vvec $ \size ptr ->
-    GL.bufferData GL.ArrayBuffer $= (size, ptr, GL.StaticDraw)
+  vbo <- loadBuffer GL.ArrayBuffer vvec
   return $ \attribIdx -> do
     GL.bindBuffer GL.ArrayBuffer $= Just vbo
     GL.vertexAttribArray (GL.AttribLocation attribIdx) $= GL.Enabled
@@ -111,11 +107,3 @@ loadVbo (VVector (vvec :: VS.Vector (V n a))) = do
           (toGLType (Proxy :: Proxy a))
           0
           nullPtr)
-  where
-    withVecSize xs cb = VS.unsafeWith xs (cb (sizeOfVec xs))
-    sizeOfVec xs = fromIntegral (sizeOf (VS.head xs) * VS.length xs)
-
-class ToGLType a where toGLType :: Proxy a -> GL.DataType
-instance ToGLType Half where toGLType _ = GL.HalfFloat
-instance ToGLType Float where toGLType _ = GL.Float
-instance ToGLType Double where toGLType _ = GL.Double
